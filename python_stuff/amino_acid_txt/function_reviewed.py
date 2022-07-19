a2552fa8d661c7229a0f5bda25099130ca3b5ae1
edenspec2 <109223428+edenspec2@users.noreply.github.com>
2022-07-19 16:18:22 +0300

    workday 1 codereview

--- python_stuff/amino_acid_txt/function_reviewed.py
+++ python_stuff/amino_acid_txt/function_reviewed.py
@@ -1,94 +1,26 @@
+import sys
+path_to_add=r'C:\Users\edenspec\Documents\GitHub'
+sys.path.insert(0, path_to_add)
+
 import pandas as pd
 import numpy as np
 import os
 
-##import learning-python.python_stuff.main.xyz_file_function as eden_script
+##import learning_python.python_stuff.main.xyz_file_function as eden_script
 ##eden_script.get_file lines()
 ##
-##from learning-python.python_stuff.main.xyz_file_function import get_file lines
+##from learning_python.python_stuff.main.xyz_file_function import get_file lines
 ##get_file_lines()
 ##
-##from learning-python.python_stuff.main.xyz_file_function import * # import all
+##from learning_python.python_stuff.main.xyz_file_function import * # import all
 ##get_file_lines()
 
 import learning_python.python_stuff.main.xyz_file_function as pl
 ##from learning-python.python_stuff.main.xyz_file_function import *
 
-def text_to_matrix(file_name):
-    """
-    a function that recieves a tabular text file and returns a matrix with it's contant .
 
-    parameters
-    ---
-    file_name:str
-    the name of the file
-
-    returns
-    ---
-    matrix:two dimentional datastructure containing the text file as strings split by ' '
-
-    examples
-    ---
-    'my_file_exmaple.pdb'
-    ...
-    ...
-    ATOM   3508  N   THR B   3     120.279 122.945 136.234  1.00 11.70           N  
-    ATOM   3509  CA  THR B   3     119.995 122.162 135.043  1.00 11.70           C  
-    ATOM   3510  C   THR B   3     120.450 120.735 135.291  1.00 11.70           C
-    ...
-    ...
-    pdb_filename='my_file_exmaple.pdb'
-    matrix=text_to_matrix(pdb_filename)
-    OUTPUT:
-    [['ATOM', '3508', 'N', 'THR', 'B', '3', '120.279', '122.945', '136.234', '1.00', '11.70', 'N'],
-    ['ATOM', '3509', 'CA', 'THR', 'B', '3', '119.995', '122.162', '135.043', '1.00', '11.70', 'C'],
-    ['ATOM', '3510', 'C', 'THR', 'B', '3', '120.450', '120.735', '135.291', '1.00', '11.70', 'C']]
-     
-    """
-    splitted_file_lines=pl.get_tabular_text_to_matrix(file_name)
-    
-    return splitted_file_lines
-
-def matrix_to_dataframe(matrix, columns=None):
-    """
-    a function that recieves a matrix and returns it as DataFrame.
-
-    parameters
-    ---
-    matrix: an array with similar data type.
-
-    columns: adds columns headlines. diffult option as none.
-
-    returns
-    ---
-    DataFrame: an array that can contain different classes of data.
-
-    examples:
-    ---
-    matrix
-    ...
-    [['ATOM', '3508', 'N', 'THR', 'B', '3', '120.279', '122.945', '136.234', '1.00', '11.70', 'N'],
-    ['ATOM', '3509', 'CA', 'THR', 'B', '3', '119.995', '122.162', '135.043', '1.00', '11.70', 'C'],
-    ['ATOM', '3510', 'C', 'THR', 'B', '3', '120.450', '120.735', '135.291', '1.00', '11.70', 'C']]
-    ...
-    pdb_file_columns=['Kind','', 'Atom name','Amino acid name', 'chain_number','place in chain','x','y','z','relative place','','Atom']
-    dataframe=matrix_to_dataframe(matrix,pdb_file_columns)
-    
-    OUTPUT:
-    Kind        Atom name Amino acid name  ...        z relative place        Atom
-0    ATOM   3508         N             THR  ...  136.234           1.00  11.70    N
-1    ATOM   3509        CA             THR  ...  135.043           1.00  11.70    C
-2    ATOM   3510         C             THR  ...  135.291           1.00  11.70    C  
-    """
-    
-    if columns:
-        database=pd.DataFrame(matrix, columns=columns)
-    else:
-        database=pd.DataFrame(matrix)
-    return database
-
-def text_to_dataframe(file_name, columns=None):
-    """
+def pdbfile_to_dataframe(pdb_filename, columns=None):
+    """UPDATED DOC
     a function that recieves a text and returns it as DataFrame.
 
     parameters
@@ -121,9 +53,10 @@ def text_to_dataframe(file_name, columns=None):
     1    ATOM   3509        CA             THR  ...  135.043           1.00  11.70    C
     2    ATOM   3510         C             THR  ...  135.291           1.00  11.70    C 
     """
-    matrix=text_to_matrix(file_name)
-    my_df=matrix_to_dataframe(matrix, columns=columns)
+    splitted_lines=pl.get_tabular_text_to_matrix(pdb_filename)
+    my_df=pl.get_dataframe_from_splitted_lines(splitted_lines, columns=columns)
     return my_df
+
 
 def pdb_filename_to_identifiers(pdb_filename):
     """
@@ -155,7 +88,7 @@ def pdb_filename_to_identifiers(pdb_filename):
     amino_acid_chain, amino_acid_name, amino_acid_place=split_result[:3]
     return amino_acid_chain, amino_acid_name, amino_acid_place
 
-def normalize_coordinates(dataframe):
+def get_normalize_coordinates(dataframe,coordinates_normalize_parameter):
     """
      a function that recieves a dataframe containing xyz coordinates of amino acids and normalizes them to origin coordinates.
 
@@ -179,201 +112,34 @@ def normalize_coordinates(dataframe):
     nomalized_dataframe=normalize_coordinates(dataframe)
 
     OUTPUT:
-        -5.702893 -5.508459  1.756151  N  
+        -5.702893 -5.508459  1.756151  N  5
         -4.299893 -5.269459  2.069151  C  
         -3.743893 -4.105459  1.260151  C
 
     """
-    avg_xyz=dataframe.astype(float).mean()
-    dataframe=(dataframe.astype(float)-np.array(avg_xyz))
+    dataframe=(dataframe.astype(float)-coordinates_normalize_parameter)
     return dataframe
 
-def customize_columns(dataframe,specific_columns):
-    """
-     a function that recieves a dataframe and specific column names and returns the dataframe with only the relevant columns.
-
-    parameters
-    ---
-    dataframe: an array that can contain different classes of data.
-
-    specific_columns:str, the specific columns we want in our new dataframe
-
-    returns
-    ---
-    dataframe: the same dataframe containing only the specified columns.
-
-    examples:
-    ---
-    my_dataframe='example_data_frame.pdb'
-    ...
-    Kind        Atom name Amino acid name  ...        z relative place        Atom
-    0    ATOM   3508         N             THR  ...  136.234           1.00  11.70    N
-    1    ATOM   3509        CA             THR  ...  135.043           1.00  11.70    C
-    2    ATOM   3510         C             THR  ...  135.291           1.00  11.70    C
-
-    new_dataframe=customize_columns(my_dataframe,specific_columns)
-
-    OUTPUT:
-    Amino acid name chain_number place in chain  ...         y         z  Atom
-0               THR            B              3  ... -5.508459  1.756151     N
-1               THR            B              3  ... -5.269459  2.069151     C
-2               THR            B              3  ... -4.105459  1.260151     C
-
-    """
-    new_dataframe=dataframe[specific_columns]
-    return new_dataframe
-
-def create_mask_from_column(dataframe,column_name,column_value):
-    """
-     a function that recieves a dataframe, a specific column name and a column value, and returns a list with True where the value is found and False otherwise .
-
-    parameters
-    ---
-    dataframe: an array that can contain different classes of data.
-    column_name:str, the specific column we want find our value in.
-    column_value: str, the specific value we search for.
-
-    returns
-    ---
-    mask: a list of True where the value is found and False otherwise
-    examples:
-    ---
-        my_dataframe'example_data_frame.pdb'
-    ...
-    Kind        Atom name Amino acid name  ...        z relative place        Atom
-    0    ATOM   3508         N             THR  ...  136.234           1.00  11.70    N
-    1    ATOM   3509        CA             THR  ...  135.043           1.00  11.70    C
-    2    ATOM   3510         C             THR  ...  135.291           1.00  11.70    C
-
-    mask=create_mask_from_column(my_dataframe,'Amino','CA')
-
-    OUTPUT:
-    0      False
-    1      True
-    2      False
-    
-    """
-    mask=dataframe[column_name]==column_value
-    return mask
-#                                      [name_1, name_2 ..] [value_1, value_2, ..]
-def create_mask_from_columns(dataframe, column_names, column_values):
-    """
-     a function that recieves a dataframe, specific columns and column values, and returns a list of the True for rows where the values were found in the colums and False otherwise  .
-
-    parameters
-    ---
-    dataframe: an array that can contain different classes of data.
-    column_names:str, the specific columns we want to find our values in.
-    column_values: str, the specific values we search for .
-    
-
-    returns
-    ---
-    total_mask: a list of the True in rows where the values were found in the colums and False otherwise
-    examples:
-    ---
-        my_dataframe='example_data_frame.pdb'
-        
-        Kind        Atom name Amino acid name  ...        z relative place        Atom
-    0    ATOM   3508         N             THR  ...  136.234           1.00  11.70    N
-    1    ATOM   3509        CA             THR  ...  135.043           1.00  11.70    C
-    2    ATOM   3510         C             THR  ...  135.291           1.00  11.70    C
-    total_mask=create_mask_from_columns(my_dataframe, column_names=['Amino acid name','Atom'], ['THR','N'])
-
-    OUTPUT:
-    0      True
-    1      False
-    2      False
-    
-        
-    """
-    mask_list=[create_mask_from_column(dataframe, column_name, column_values[column_index]) for column_index, column_name in enumerate(column_names)]
-    total_mask=mask_list[0]
-    for mask in mask_list[1:]:
-        total_mask=total_mask&mask
-    return total_mask
-
-def dataframe_to_xyz(dataframe, output_name='xyz file', comment_line='XYZ file'):
-    """
-
-     a function that recieves a dataframe, output name, and comment line and creates a xyz type file.
-     
-    parameters
-
-    ---
-
-    dataframe: an array that can contain different classes of data, needs to be 4 colums to run.
-
-    output_name:str, the name for the file created.
-
-    comment_line: str, the headline of the file .
-    ---
-
-    examples:
-    ---
-    """
-    output_filename=output_name
-    number_of_atoms=dataframe.shape[0]
-    atoms_np_array=dataframe.to_numpy()
-    with open(output_filename, 'w') as xyz_file:
-        xyz_file.write("{}\n{}\n".format(number_of_atoms, comment_line))
-        for atom_np_array in atoms_np_array:
-           xyz_file.write("{:4} {:11.6f} {:11.6f} {:11.6f}\n".format(*atom_np_array))
-
-def change_filetype (filename,new_type):
-    """
-    a function that recieves a file name, and a new type, and changes the type-ending of the file's name to the new one.
-
-    parameters
-    ---
-    filename: str, the file we want to change
-
-    new_type:str, the new ending we want for the file
-
-    returns
-    ---
-    the same file name with a new type ending
-
-    examples
-    ---
-    filename='B_THR_127_5Angs_noHOH.pdb'
-    new_filename=change_filetype(filename,'xyz')
-    OUTPUT:'B_THR_127_5Angs_noHOH.xyz'
-    
-    """
-    split_result=filename.split('.')
-    if new_type.startswith('.'):
-        new_filename=split_result[0]+new_type
-    else:
-        new_filename=split_result[0]+'.'+new_type
-    return new_filename
-
-            
 
 pdb_file_columns=['Kind','', 'Atom name','Amino acid name', 'chain_number','place in chain','x','y','z','relative place','','Atom']
 specific_columns=['Amino acid name','chain_number','place in chain','x','y','z', 'Atom']
 column_names=['chain_number','Amino acid name','place in chain']
 
 if __name__=='__main__':
-    
-    list_of_files=os.listdir(r'C:\Users\edenspec\Documents\GitHub\learning-python\python stuff\amino acid txt files')
+    list_of_files=os.listdir(r'C:\Users\edenspec\Documents\GitHub\learning_python\python_stuff\amino_acid_txt')
     list_of_pdb_files=[filename for filename in list_of_files if filename.endswith('pdb')]
     print(list_of_pdb_files)
     for pdb_filename in list_of_pdb_files:
-        my_database=text_to_dataframe(pdb_filename,columns=pdb_file_columns)
+        my_database=pdbfile_to_dataframe(pdb_filename,columns=pdb_file_columns)
+        normlize_point=my_database[['x','y','z']].astype(float).mean()
         amino_acid_chain, amino_acid_name, amino_acid_place=pdb_filename_to_identifiers(pdb_filename)
-        new_database=customize_columns(my_database,specific_columns)
-        total_mask=create_mask_from_columns(new_database, column_names, [amino_acid_chain, amino_acid_name, amino_acid_place])
-        new_database.loc[:, ['x','y','z']]=normalize_coordinates(new_database[['x','y','z']].copy())
-        output_filename=change_filetype(pdb_filename, 'xyz')
-        dataframe_to_xyz(new_database[total_mask][['Atom','x','y','z']],output_filename)
-##        print(total_mask)
-    matrix=text_to_matrix('B_THR_127_5Angs_noHOH.pdb')
-    dataframe=matrix_to_dataframe(matrix,pdb_file_columns)
-    dataframe[['x','y','z']]=normalize_coordinates(new_database[['x','y','z']])
-##    print(customize_columns(dataframe,specific_columns))
-##    print(list_of_pdb_files)
-    mask1=create_mask_from_column(dataframe,'Atom','H')
+        new_database=pl.customize_columns(my_database,specific_columns)
+        total_mask=pl.search_multiple_vaules_in_columns(new_database, column_names, [amino_acid_chain, amino_acid_name, amino_acid_place])
+        new_database.loc[:, ['x','y','z']]=pl.get_normalize_coordinates(new_database[['x','y','z']].copy(),normlize_point)
+        output_filename=pl.change_filetype(pdb_filename, 'xyz')
+        pl.dataframe_to_xyz(new_database[total_mask][['Atom','x','y','z']],output_filename)
+        print(new_database[total_mask][['Atom','x','y','z']])
+
     
 
 
