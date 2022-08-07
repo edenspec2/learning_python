fa1d0cfaf6786e7b3521b6299824b4e368ef9374
edenspec2 <edenspec@post.bgu.ac.il>
2022-08-07 16:42:56 +0300

    latest update

--- qa_tester_for_project/comp_set.py
+++ qa_tester_for_project/comp_set.py
@@ -1,10 +1,10 @@
 import numpy as np
 
+def align_molecules(b,c):
+    return b*c
+
 def get_xyz_df(x):
     return x+5
-
-def align_molecules(b,c):
-    return b+c
 
 def run_calculation(z):
     return np.array([z, z+1, z+2])
@@ -16,8 +16,8 @@ if __name__=='__main__':
     print('all good')
 
 
-num1=get_xyz_df(2)
-num2=align_molecules(num1,5)
+num1=align_molecules(2,3)
+num2=get_xyz_df(num1)
 num3=(run_calculation(num2))
 num4=get_ml_model(num3)
 print(num4)
