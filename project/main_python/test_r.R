ef4bba8b21178abc30a5abc49ed28ff84a75dde0
edenspec2 <109223428+edenspec2@users.noreply.github.com>
2022-08-11 12:30:33 +0300

    update

--- project/main_python/test_r.R
+++ project/main_python/test_r.R
@@ -4,10 +4,8 @@ library('plyr')
 
 source("C:/Users/עדן/Documents/GitHub/learning_python/project/moleculaR-main/R_scripts/code/comp.set.1.1.R")
 xyz_file_generator("C:/Users/עדן/Documents/GitHub/learning_python/project/main_python")
-folder <- "mortalkombat"
-
-setwd("../")
-dir.create(paste("../", folder, sep = ''), showWarnings = F)
+setwd("C:/Users/עדן/Documents/GitHub/learning_python/project/main_python")
+swapper('csv_file_for_r_1.xyz','2 5')
 
 
 
